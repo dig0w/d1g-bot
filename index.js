44406ed904955aa25e540bb3d33a4ad3f8827980
dig0w <dig0w.yt@gmail.com>
2021-11-23 15:32:50 +0000

    main

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    const ping = new Date();
+    ping.setHours(ping.getHours()-3);
+    res.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS,Discord.Intents.FLAGS.GUILD_MEMBERS,Discord.Intents.FLAGS.GUILD_BANS,Discord.Intents.FLAGS.GUILD_INVITES,Discord.Intents.FLAGS.GUILD_VOICE_STATES,Discord.Intents.FLAGS.GUILD_PRESENCES,Discord.Intents.FLAGS.GUILD_MESSAGES,Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
