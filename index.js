5f22a462a77ad2fe1fb07ff592549fc61516ed23
dig0w <67347822+dig0w@users.noreply.github.com>
2022-07-29 17:01:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
 
-const app = require('express')();
-app.get('/', (req, res) => {
-    const ping = new Date();
-    ping.setHours(ping.getHours()-3);
-    res.sendStatus(200);
-});
-app.listen(process.env.PORT);
-
 const discord = require('discord.js');
 
-const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_BANS, discord.Intents.FLAGS.GUILD_INVITES, discord.Intents.FLAGS.GUILD_VOICE_STATES, discord.Intents.FLAGS.GUILD_PRESENCES, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_VOICE_STATES, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.queue = new Map();
 
