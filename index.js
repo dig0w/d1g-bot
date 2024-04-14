180f465e0fb374b5f4fdf8183f52913b0bf30e48
dig0w <67347822+dig0w@users.noreply.github.com>
2024-04-14 11:44:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 require("dotenv").config();
 
-// const app = require("express")();
-// const port = 8080;
-// app.get("/", (req, res) => {
-//     const ping = new Date();
-//     ping.setHours(ping.getHours()-3);
-//     res.sendStatus(202);
-// });
-
 const Discord = require("discord.js");
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.GuildEmojisAndStickers, Discord.GatewayIntentBits.GuildIntegrations, Discord.GatewayIntentBits.GuildInvites, Discord.GatewayIntentBits.GuildMembers, Discord.GatewayIntentBits.GuildMessageReactions, Discord.GatewayIntentBits.GuildMessageTyping, Discord.GatewayIntentBits.GuildMessages, Discord.GatewayIntentBits.GuildModeration, Discord.GatewayIntentBits.GuildPresences, Discord.GatewayIntentBits.GuildScheduledEvents, Discord.GatewayIntentBits.GuildVoiceStates, Discord.GatewayIntentBits.GuildWebhooks, Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.MessageContent] });
@@ -18,5 +10,3 @@ client.commands = new Discord.Collection();
 require("./handler.js")(client);
 
 client.login(process.env.clientToken);
-
-// app.listen(process.env.PORT || port, () => console.log(`🟩 App listening on port: ${process.env.PORT || port}`) );
